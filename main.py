4447040445a29aaf3e85b3b3fefa0865fec01cbe
Jaime Loring Castillo <trjlca@gmv.es>
2020-10-15 12:17:25 +0200

    Base de descripcion

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 ### 15/10/2020
+
+''' Sistema de control de gastos:
++ CAPACIDADES:
++ USUARIOS:
++ FORMA DE USO:
+ 
+ Espero:
+ + Recordar bases de datos
+ + Aprender Git y GitHub
+ + Aprender GUIs
+ + Hacer algo útil
+'''
